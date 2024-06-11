0fdf585fef0d9003579ca2906d7d2a0deff77808
andrescuel <camilo.andcu@gmail.com>
2024-06-11 02:30:42 -0500

    Configuración de NGINX actualizada

--- backend/src/main/java/dev/sena/credikfam/CorsConfig.java
+++ backend/src/main/java/dev/sena/credikfam/CorsConfig.java
@@ -10,9 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
